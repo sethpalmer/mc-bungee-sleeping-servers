a1dd6f27e3a95795942b4a8470e6f59a8f45d7ee
Seth Palmer <80277024+sethpalmer@users.noreply.github.com>
2024-01-28 16:54:43 -0700

    config in yaml

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const spawn = require('child_process').spawn
 const mc = require('minecraft-protocol')
+const jsyaml = require('js-yaml')
+const fs = require('fs');
+
+try {
+  const doc = jsyaml.load(fs.readFileSync('sleeping-server-config.yml', 'utf8'));
+  console.log(doc);
+
+  const serverOpts = doc.servers
+  const bungee = doc.bungee
+
+  const cmdArgs = `-Xmx${bungee.memory}M -Xms${bungee.memory}M -jar ${bungee.binary} nogui`.split(' ')
+  const bungeeProc = spawn('java', cmdArgs, {
+    cwd: bungee.directory
+  })
+  bungeeProc.stdout.on('data', function(data) {
+    console.log(data.toString());
+  });
+
+  for(var serverOpt in serverOpts){
+    initServer(serverOpts[serverOpt])
+  }
+} catch (e) {
+  console.log(e);
+}
 
 function initServer(serverOpt) {
   const options = {
     motd: 'sleeping server',
-    'max-players': 5,
+    maxPlayers: 5,
     port: serverOpt.port,
     'online-mode': false,
     beforeLogin: function(client) {
@@ -12,16 +36,10 @@ function initServer(serverOpt) {
       server.close()
       console.log("closed sleeping server for", serverOpt.directory)
       const cmdArgs = `-Xmx${serverOpt.memory}M -Xms${serverOpt.memory}M -jar ${serverOpt.binary} nogui`.split(' ')
-      
-      // var mcProcess = spawn('ls', {
-      //   // To receive stop from kill on Windows
-      //   stdio: ["overlapped", "inherit", "inherit"],
-      //   cwd: serverOpt.directory,
-      // })
     
       console.log("Starting", serverOpt.directory)
       const mcProcess = spawn('java', cmdArgs, {
-        cwd: `D:\\Minecraft\\${serverOpt.directory}`
+        cwd: serverOpt.directory
       })
 
       mcProcess.on("close", (code) => {
@@ -50,74 +68,3 @@ function initServer(serverOpt) {
     console.log(serverOpt.directory, 'listening on port', server.socketServer.address().port)
   })
 }
-
-function wakeUpServer(serverOpt){
-  const cmdArgs = `java -Xmx${serverOpt.memory}M -Xms${serverOpt.memory}M -jar ${serverOpt.binary} nogui`
-  
-  var mcProcess = spawn(cmdArgs, {
-    // To receive stop from kill on Windows
-    stdio: ["overlapped", "inherit", "inherit"],
-    cwd: serverOpt.directory,
-  })
-
-  mcProcess.stdout.on('data', function(data) {
-    console.log(serverOpt.directory, data.toString());
-  });
-}
-
-const serverOpts = [
-  {
-    port: 25587,
-    directory: 'hub-server',
-    binary: 'paper-1.20.2-318.jar',
-    memory: 512
-  },
-  {
-    port: 25592,
-    directory: 'survival-server',
-    binary: 'paper-1.20.2-318.jar',
-    memory: 4096
-  },
-  {
-    port: 25593,
-    directory: 'squirrel',
-    binary: 'paper-1.20.2-318.jar',
-    memory: 512
-  },
-  {
-    port: 25595,
-    directory: 'breezecraft',
-    binary: 'paper-1.20.2-318.jar',
-    memory: 512
-  },
-  {
-    port: 25596,
-    directory: 'seth-mimi-luke-clarissa',
-    binary: 'paper-1.20.2-318.jar',
-    memory: 512
-  },
-  {
-    port: 25597,
-    directory: 'cedar-server',
-    binary: 'paper-1.20.2-318.jar',
-    memory: 512
-  },
-  {
-    port: 25598,
-    directory: 'creative-server',
-    binary: 'paper-1.20.2-318.jar',
-    memory: 512
-  }
-]
-
-for(var serverOpt in serverOpts){
-  initServer(serverOpts[serverOpt])
-}
-
-const cmdArgs = `-Xmx512M -Xms512M -jar BungeeCord.jar nogui`.split(' ')
-const bungeeProc = spawn('java', cmdArgs, {
-  cwd: `D:\\Minecraft\\bungeecord-server`
-})
-bungeeProc.stdout.on('data', function(data) {
-  console.log(data.toString());
-});
